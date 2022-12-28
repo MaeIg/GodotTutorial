b01b04168e570fc447c83e371a1915b14e080670
MaeIg <larochmael@hotmail.fr>
2022-12-28 17:25:18 +0100

    feat(2d): connect Main scene and HUD

--- 2d/scenes/Main.gd
+++ 2d/scenes/Main.gd
@@ -46,19 +46,24 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
 
+func _on_HUD_start_game():
+	new_game()
+
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$StartTimer.start()
-
-
