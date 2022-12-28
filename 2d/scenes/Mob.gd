32665c80b4beae1e92578d1efb940e5e12e96c2b
MaeIg <larochmael@hotmail.fr>
2022-12-28 16:04:56 +0100

    feat(2d): play a random animation when Mob is ready

---
+++ 2d/scenes/Mob.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+# Declare member variables here
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
