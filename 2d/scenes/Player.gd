8a27ecf24501d1834c00984ed338add72c915c4d
Maël Giret <40902940+MaeIg@users.noreply.github.com>
2022-12-28 18:47:16 +0100

    2d tutorial (#1)
    * feat(2d): create 2d project
    
    * chore(2d): add assets
    
    * feat(2d): change display size
    
    * feat(2d): create Player scene
    
    * feat(2d): add animated sprites to Player scene
    
    * feat(2d): add a hitbox to Player scene
    
    * feat(2d): configure inputs in settings
    
    * feat(2d): add a script to Player scene
    
    * feat(2d): handle movement in Player scene
    
    * feat(2d): handle Player animation
    
    * feat(2d): hide Player when scene is ready
    
    * feat(2d): send hit signal when Player is hit
    
    * feat(2d): add a start method to reset the Player when starting a new game
    
    * feat(2d): add Mob scene
    
    * feat(2d): play a random animation when Mob is ready
    
    * feat(2d): remove Mob when it is out of the screen
    
    * feat(2d): create Main scene
    
    * feat(2d): add random Mob spawns
    
    * feat(2d): create game logic
    
    * feat(2d): set main scene
    
    * feat(2d): create HUD
    
    * feat(2d): add HUD logic
    
    * feat(2d): connect Main scene and HUD
    
    * fix(2d): remove all mobs when starting a new game
    
    * feat(2d): add background color
    
    * feat(2d): add sound to the game
    
    background music + death sound
    
    * feat(2d): add a shortcut to start the game

---
+++ 2d/scenes/Player.gd
@@ -0,0 +1,60 @@
+extends Area2D
+
+signal hit
+
+# Declare member variables here.
+export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+		# $ is shorthand for get_node(). 
+		# So in the code below, $AnimatedSprite.play() is the same as get_node("AnimatedSprite").play().
+		$AnimatedSprite.play()
+		
+		position += velocity * delta
+		position.x = clamp(position.x, 0, screen_size.x)
+		position.y = clamp(position.y, 0, screen_size.y)
+		
+		if velocity.x != 0:
+			$AnimatedSprite.animation = "walk"
+			$AnimatedSprite.flip_v = false
+			$AnimatedSprite.flip_h = velocity.x < 0
+		elif velocity.y != 0:
+			$AnimatedSprite.animation = "up"
+			$AnimatedSprite.flip_v = velocity.y > 0
+	else:
+		$AnimatedSprite.stop()
+
+
+func _on_Player_body_entered(_body):
+	hide() # Player disappears after being hit.
+	emit_signal("hit")
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
