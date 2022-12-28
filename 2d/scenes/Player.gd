91d8b13bf9d383d38f8a239628534ac1ba99befc
MaeIg <larochmael@hotmail.fr>
2022-12-28 12:12:29 +0100

    feat(2d): handle movement in Player scene

--- 2d/scenes/Player.gd
+++ 2d/scenes/Player.gd
@@ -12,5 +12,27 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+		# $ is shorthand for get_node(). 
+		# So in the code below, $AnimatedSprite.play() is the same as get_node("AnimatedSprite").play().
+		$AnimatedSprite.play()
+		
+		position += velocity * delta
+		position.x = clamp(position.x, 0, screen_size.x)
+		position.y = clamp(position.y, 0, screen_size.y)
+	else:
+		$AnimatedSprite.stop()
